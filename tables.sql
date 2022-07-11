0dc7fb0c91700a779cd1d1f372faeeb4ae9f5ba0
Karen Kubo <karennckubo@gmail.com>
2022-07-11 13:13:38 -0300

    tables.sql

--- tables.sql
+++ tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE
         gears INT NOT NULL,
         brand VARCHAR(255) NOT NULL,
         model VARCHAR(255) NOT NULL,
-        price INT NOT NULL
+        price DECIMAL(10,2) NOT NULL
     );
 SELECT * FROM `Hubees_Shop`;
 
